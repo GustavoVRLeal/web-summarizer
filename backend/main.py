d11b6719c28804742c771ce7cff680ee9d53b556
Gustavo Leal <gustavovallim75@gmail.com>
2026-01-20 00:21:36 -0300

    Add summarize endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI(title="Web Summarizer API")
+
+class TextRequest(BaseModel):
+    text: str
 
 @app.get("/")
 def read_root():
-    return{"message": "API do Web Summarizer está rodando 🚀"}
+    return {"message": "API do Web Summarizer está rodando 🚀"}
+
+@app.post("/summarize")
+def summarize(request: TextRequest):
+    text = request.text
+
+    # Resumo fake (por enquanto)
+    summary = text[:100] + "..." if len(text) > 100 else text
+
+    return {
+        "summary": summary
+    }
